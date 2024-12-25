dfe67a9ad844f89e1c4c4156a0d9183433c3c8cf
sdevsantiago <147100454+sdevsantiago@users.noreply.github.com>
2024-12-25 13:28:14 +0100

    📦 chore: added MLX42_int header file, all headers related to MLX42 are now stored in a folder

--- includes/fdf.h
+++ includes/fdf.h
@@ -6,17 +6,17 @@
 /*   By: sede-san <sede-san@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/23 11:55:10 by sede-san          #+#    #+#             */
-/*   Updated: 2024/12/23 12:13:54 by sede-san         ###   ########.fr       */
+/*   Updated: 2024/12/23 23:29:06 by sede-san         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FDF_H
 # define FDF_H
 
+# include "MLX42/MLX42.h"
 # include <math.h>
 # include <stdio.h>		// perror
 # include <stdlib.h>	// exit
 # include <string.h>	// strerror
-# include "MLX42.h"
 
 #endif
