493bc7e49c0478dc51538c0ad2388cbf9f658f6f
sede-san <sede-san@student.42madrid.com>
2025-01-09 20:10:25 +0100

    🚧 feat: program now closes when ESC key is pressed (WIP, segmentation fault)

--- src/fdf.c
+++ src/fdf.c
@@ -6,20 +6,26 @@
 /*   By: sede-san <sede-san@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/23 18:48:02 by sede-san          #+#    #+#             */
-/*   Updated: 2024/12/30 02:44:58 by sede-san         ###   ########.fr       */
+/*   Updated: 2025/01/09 20:08:19 by sede-san         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/fdf.h"
 
+void	check_keypress(mlx_key_data_t key, void *param)
+{
+	// If ESC is pressed, kill the window
+	if (key.key == MLX_KEY_ESCAPE && key.action == MLX_PRESS)
+		mlx_terminate(param);
+}
+
 int main(void)
 {
 	mlx_t			*mlx;
-	// mlx_texture_t	*icon;
 
 	mlx = mlx_init(256, 256, "Fdf by sede-san", true);
+	mlx_key_hook(mlx, &check_keypress, (void *)mlx);
 	mlx_loop(mlx);
 	mlx_terminate(mlx);
-	
 	return (0);
 }
